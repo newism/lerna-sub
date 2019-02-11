19885a1d9637492309bc17abed6a624abcddd0b6
Leevi Graham <leevi@newism.com.au>
2019-02-11 16:23:37 +1100

    Added webpack encore for a test

---
+++ public/build/app.js
@@ -0,0 +1,37 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _newism_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @newism/common */ "./node_modules/@newism/common/lib/common.js");
+/* harmony import */ var _newism_common__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_newism_common__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }),
+
+/***/ "./node_modules/@newism/common/lib/common.js":
+/*!***************************************************!*\
+  !*** ./node_modules/@newism/common/lib/common.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = common;
+
+function common() {
+}
+
+
+/***/ })
+
+},[["./assets/js/app.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvYXBwLmpzIiwid2VicGFjazovLy8uL25vZGVfbW9kdWxlcy9AbmV3aXNtL2NvbW1vbi9saWIvY29tbW9uLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQTtBQUFBO0FBQUE7Ozs7Ozs7Ozs7Ozs7QUNBYTs7QUFFYjs7QUFFQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnQG5ld2lzbS9jb21tb24nO1xuXG4iLCIndXNlIHN0cmljdCc7XG5cbm1vZHVsZS5leHBvcnRzID0gY29tbW9uO1xuXG5mdW5jdGlvbiBjb21tb24oKSB7XG59XG4iXSwic291cmNlUm9vdCI6IiJ9
